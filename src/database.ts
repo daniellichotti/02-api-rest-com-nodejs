79136027ba452474257b6ef8ba7cf01a0e3c6459
daniellichotti <danielclichotti@gmail.com>
2024-02-02 16:23:54 -0300

    Banco de Dados - Criando tabela de transacoes

--- src/database.ts
+++ src/database.ts
@@ -3,11 +3,11 @@ import { Knex, knex as setupKnex } from 'knex'
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/ap.db',
+    filename: './db/app.db',
   },
   useNullAsDefault: true,
   migrations: {
-    extension: '.ts',
+    extension: 'ts',
     directory: './db/migrations',
   },
 }
