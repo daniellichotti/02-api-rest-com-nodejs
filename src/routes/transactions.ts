2c8e8b404d764fd0332ecc7d5563567d43792f7b
daniellichotti <danielclichotti@gmail.com>
2024-02-06 16:16:19 -0300

    Implementando as Rotas - Criação de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,25 +1,26 @@
 import { FastifyInstance } from 'fastify'
-import { knex } from '../database'
+import knex from 'knex'
+import { randomUUID } from 'node:crypto'
+import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    /*
-        const tables = await knex('sqlite_schema').select('*')
-      
-        const transaction = await knex('transactions')
-        .insert({
-          id: crypto.randomUUID(),
-          title: 'transacao de teste',
-          amount: 1000,
-        })
-        .returning('*')
-        const transaction = await knex('transactions').select('*')
-        const transaction = await knex('transactions')
-        .where('amount', 1000)
-        .select('*')
-        */
-    const transaction = await knex('transactions').select('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transaction
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
